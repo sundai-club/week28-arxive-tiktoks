a830b3e17abd13221dbb50a0447dd7a8e819fe6d
Mihir Athale <athale.m@northeastern.com>
2024-08-11 15:48:26 -0400

    added image extractor

--- content_generation/src/pdf_parser.py
+++ content_generation/src/pdf_parser.py
@@ -1,10 +1,48 @@
 import PyPDF2
-import scipdf
+import os.path as op
+import os
+import subprocess
+
+PDF_FIGURES_JAR_PATH = "./pdffigures2/pdffigures2-assembly-0.0.12-SNAPSHOT.jar"
+
+
+def parse_figures(pdf_folder, jar_path, output_folder, resolution=300):
+    if not op.isdir(output_folder):
+        os.makedirs(output_folder)
+
+    data_path = op.join(output_folder, "data")
+    figure_path = op.join(output_folder, "figures")
+    if not op.exists(data_path):
+        os.makedirs(data_path)
+    if not op.exists(figure_path):
+        os.makedirs(figure_path)
+
+    if op.isdir(data_path) and op.isdir(figure_path):
+        args = [
+            "java",
+            "-jar",
+            jar_path,
+            pdf_folder,
+            "-i",
+            str(resolution),
+            "-d",
+            op.join(op.abspath(data_path), ""),
+            "-m",
+            op.join(op.abspath(figure_path), ""),
+        ]
+        _ = subprocess.run(
+            args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=20
+        )
+        return True
+    return False
+
 
 
 def get_all_images(pdf_file_path):
-    scipdf.parse_figures(pdf_file_path, output_folder=pdf_file_path.split('.pdf')[0] + '_figures')
-    return pdf_file_path.split('.pdf')[0] + '_figures'
+    output_folder = pdf_file_path.split('.pdf')[0]
+    if not parse_figures(pdf_file_path, PDF_FIGURES_JAR_PATH, output_folder):
+        return None
+    return output_folder
 
 
 def parse_pdf(pdf_file_path):
